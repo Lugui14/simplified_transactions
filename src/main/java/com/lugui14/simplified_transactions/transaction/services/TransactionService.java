a0300b6f4482b7100b483b314310dc96febbca6e
luiz <luiz.lopes@desbravador.com.br>
2025-03-25 18:06:06 -0300

    feat: transaction start

--- src/main/java/com/lugui14/simplified_transactions/transaction/services/TransactionService.java
+++ src/main/java/com/lugui14/simplified_transactions/transaction/services/TransactionService.java
@@ -1,7 +1,42 @@
 package com.lugui14.simplified_transactions.transaction.services;
 
+import com.lugui14.simplified_transactions.transaction.domain.Transaction;
+import com.lugui14.simplified_transactions.transaction.domain.User;
+import com.lugui14.simplified_transactions.transaction.domain.dtos.TransactionDto;
+import com.lugui14.simplified_transactions.transaction.domain.repositories.TransactionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final UserService userService;
+
+    public TransactionService(
+            TransactionRepository transactionRepository,
+            UserService userService
+    ) {
+        this.transactionRepository = transactionRepository;
+        this.userService = userService;
+    }
+
+    @Transactional
+    public Transaction createTransaction(TransactionDto dto) {
+        User userFrom = userService.findById(dto.payer());
+        User userTo = userService.findById(dto.payee());
+
+        Transaction transaction = Transaction.builder()
+                .userFrom(userFrom)
+                .userTo(userTo)
+                .amount(dto.value())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+
+
+        return transactionRepository.save(transaction);
+    }
 }
