e8ec91735d93bca1388243b2be1256ada6946281
luiz <luiz.lopes@desbravador.com.br>
2025-03-28 10:19:29 -0300

    feat: transaction use case

--- src/main/java/com/lugui14/simplified_transactions/transaction/domain/dtos/TransactionDto.java
+++ src/main/java/com/lugui14/simplified_transactions/transaction/domain/dtos/TransactionDto.java
@@ -1,6 +1,12 @@
 package com.lugui14.simplified_transactions.transaction.domain.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record TransactionDto(BigDecimal value, Integer payer, Integer payee) {
+public record TransactionDto(
+        @NotNull(message = "Inconsistent data, value is null") BigDecimal value,
+        @NotNull(message = "Inconsistent data, payer is null") Integer payer,
+        @NotNull(message = "Inconsistent data, payee is null") Integer payee
+) {
 }
--- src/main/java/com/lugui14/simplified_transactions/transaction/domain/repositories/TransactionRepository.java
+++ src/main/java/com/lugui14/simplified_transactions/transaction/domain/repositories/TransactionRepository.java
@@ -1,7 +1,10 @@
 package com.lugui14.simplified_transactions.transaction.domain.repositories;
 
 import com.lugui14.simplified_transactions.transaction.domain.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    Page<Transaction> getTransactionsByUserFromIdOrUserToId(Integer userFromId, Integer userToId, Pageable pageable);
 }
